0c5bd51484115823269a013f224af097b6a35d73
Test User <test@example.com>
2026-08-19 13:41:25 +0530

    Allow Render frontend CORS

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 app.use(
     cors({
-        origin: "http://localhost:5173"
+        origin: ["http://localhost:5173", "https://balancesheet-frontend.onrender.com"]
     })
 );
 
@@ -68,3 +68,5 @@ app.listen(PORT, () => {
     );
 
 });
+
+
